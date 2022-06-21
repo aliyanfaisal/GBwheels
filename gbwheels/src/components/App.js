b6ee1608aa3995b83893a95078d4a4eeeed14bfb
ijlalansari1122 <ansariijlal90@gmail.com>
2022-06-21 14:22:02 +0500

    navbar was designed and file system was set

---
+++ gbwheels/src/components/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+<p>loem as as  asida as asals
+</p>
+
+      <div>
+
+<h1>hello there this is gbwheels</h1>
+        
+      </div>
+
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ gbwheels/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import  "../../src/components/Nav"
+import   "./Nav.css"
+
+function  Nav() {
+    return(
+<div className="menu"> <nav>  <ul>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Blog</a></li>
+          </ul></nav>     
+</div>
+     
+    )
+
+
+}
+export default Nav;
