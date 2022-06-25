a83b0472a266dfd708e1613f46375850ccb78036
ijlalansari1122 <ansariijlal90@gmail.com>
2022-06-25 14:15:21 +0500

    asset folder was updated

---
+++ gbwheels/src/components/assets/Div1.js
@@ -0,0 +1,24 @@
+function Service() {
+
+return(
+
+<div classname= "service">
+
+<div> <h1> Cars Inspection</h1>
+<p>lorem as asljasd alkjs aksjnas alksdasl lkjasd asndl aoisd kjlan kjandlk alkndalknd alknalkd alksdnalsn  asa asxkaslkx lkasxalksnx lkansxlka lkasnlkas</p>
+
+</div>
+<div></div>
+<div></div>
+
+
+
+
+</div>
+
+
+)
+
+
+    
+}
