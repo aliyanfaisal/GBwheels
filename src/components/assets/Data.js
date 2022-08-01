abe112cb1f53975cb1adbc7c9e2377c51ef603d3
ijlalansari1122 <ansariijlal90@gmail.com>
2022-08-01 22:22:18 +0500

    filter is operational

--- src/components/assets/Data.js
+++ src/components/assets/Data.js
@@ -5,6 +5,12 @@ import{Col} from "react-bootstrap";
 // import {Button} from "react-bootstrap";
 import './Data.css';
 function Filter() {
+  function priceChange(event){  
+      var value = event.target.value;
+      document.querySelector("#price_span").innerHTML=value
+  }
+
+
   return(
 <Container className="pb-3 " >
 <Row>
@@ -16,27 +22,25 @@ function Filter() {
 
 <div >
 
-<span>Select Year</span>
+<span>Select Brand</span>
 <br/>
 <br/>
-<select name="year" id="year" value= "year">
-<option value="tesla">-Select-</option>
+<select name="brand" id="brand" required>
+<option value="">-Select-</option>
 
-<option value="2009">2021</option>
-<option value="hyundai">2001</option>
-<option value="audi">2003</option>
-<option value="honda">2005</option>
-<option value="honda">2008</option>
-<option value="honda">2009</option>
-<option value="honda">2012</option>
+<option value="tesla">Tesla</option>
+<option value="hyundai">hyundai</option>
+<option value="audi">audi</option>
+<option value="honda">honda civic</option>
+<option value="Gli">gli</option>
 
 </select>
 </div>
 <div ><span>Select Make</span>
 <br/>
 <br/>
-<select name="Tesla" id="tesla" value= "tesla">
-<option value="tesla">-Select-</option>
+<select name="Tesla" id="tesla"  required>
+<option value="">-Select-</option>
 
 
 <option value="tesla">Tesla</option>
@@ -53,8 +57,8 @@ function Filter() {
 <span>Select Model</span>
 <br/>
 <br/>
-<select name="Tesla" id="tesla" value= "tesla">
-<option value="tesla">-Select-</option>
+<select name="Tesla" id="tesla" required>
+<option value="">-Select-</option>
 
 <option value="tesla">Tesla</option>
 <option value="hyundai">Hyundai</option>
@@ -67,12 +71,14 @@ function Filter() {
 <div >
 
   
-<span>Select Price:12120pkr </span>
+<span>Select Price: <span id="price_span"></span> </span>
 <br/>
 <br/>
-<input type="range" name="price" id="price" value="200"  maxlength="9000000" minlength="100000" />
+
+  <input type="range" id="vol" name="vol" min="1000" max="500000" onChange={priceChange}/>
+
 <br/>
-<Button className=" bg-secondry mt-4 ">Select Inventory  </Button>
+<Button className=" bg-secondry mt-4 ">Filter</Button>
 </div>
 
 
@@ -81,17 +87,17 @@ function Filter() {
 <span>Select Year</span>
 <br/>
 <br/>
-<select name="year" id="year" value= "year">
+<select name="year" id="year"  required>
 <option value="tesla">-Select-</option>
 
 
 <option value="2009">2021</option>
-<option value="hyundai">2001</option>
-<option value="audi">2003</option>
-<option value="honda">2005</option>
-<option value="honda">2008</option>
-<option value="honda">2009</option>
-<option value="honda">2012</option>
+<option value="2001">2001</option>
+<option value="2003">2003</option>
+<option value="2005">2005</option>
+<option value="2008">2008</option>
+<option value="2009">2009</option>
+<option value="2012">2012</option>
 
 </select>
 </div>
@@ -100,9 +106,8 @@ function Filter() {
   <span>Select Condition</span>
 <br/>
 <br/>
-<select name="Tesla" id="tesla" value= "tesla">
-{/* <option value="tesla">Please select Condition</option> */}
-<option value="tesla">-Select-</option>
+<select name="select" id="select1" required>
+<option value="">-Select-</option>
 
 <option value="tesla">Certified</option>
 <option value="hyundai">NCP</option>
@@ -119,13 +124,13 @@ function Filter() {
 <span>Select Mileage</span>
 <br/>
 <br/>
-<select name="Tesla" id="tesla" value= "tesla">
+<select name="Tesla" id="tesla" >
 <option value="tesla">-Select-</option>
 
-<option value="tesla">10000</option>
-<option value="hyundai">120000</option>
-<option value="audi">12123</option>
-<option value="honda">22021</option>
+<option value="10000">10000</option>
+<option value="120000">120000</option>
+<option value="12123">12123</option>
+<option value="22021">22021</option>
 
 </select>
 
